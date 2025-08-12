bd774af677fcad54fbdbc4af3bc1c8240ab73465
You Song Hou <ysongweb3@gmail.com>
2025-08-12 00:27:02 -0400

    chore: bring back connect wallet button

--- react/src/components/ConnectMenu.tsx
+++ react/src/components/ConnectMenu.tsx
@@ -5,6 +5,7 @@ import {
   useAccount,
   useChains,
   useChainId,
+  useConnect,
   useSwitchChain,
 } from "wagmi";
 // import { usePrivy } from '@privy-io/react-auth';
@@ -20,7 +21,7 @@ const supportedNetworks = [
 export function ConnectMenu() {
   // const { authenticated, login } = usePrivy();
   const { isConnected, address } = useAccount();
-  // const { connect, connectors } = useConnect();
+  const { connect, connectors } = useConnect();
   const { switchChain } = useSwitchChain();
  
   const chains = useChains();
@@ -94,12 +95,12 @@ export function ConnectMenu() {
 
   return (
     <div className="flex justify-center gap-4 py-4 bg-blue-300">
-      {/* <button
-        className="bg-indigo-600 hover:bg-indigo-700 text-white font-semibold px-6 py-2 rounded-lg shadow transition hidden"
-        onClick={() => connect({ connector: connectors[isMiniApp ? 0 : 1] })}
+      <button
+        className="bg-indigo-600 hover:bg-indigo-700 text-white font-semibold px-6 py-2 rounded-lg shadow transition"
+        onClick={() => connect({ connector: connectors[0] })}
       >
         Connect Wallet Only
-      </button> */}
+      </button>
     
       {/* <button
         className="bg-red-500 hover:bg-red-600 text-white font-semibold px-6 py-2 rounded-lg shadow transition"
