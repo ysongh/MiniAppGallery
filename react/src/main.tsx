f732488daba3298a7c99503263db028f0cf07ccf
You Song Hou <ysongweb3@gmail.com>
2025-08-13 22:17:29 -0400

    refactor: update theme.ts

--- react/src/main.tsx
+++ react/src/main.tsx
@@ -6,21 +6,13 @@ import { WagmiProvider } from '@privy-io/wagmi';
 
 import App from "./App.tsx";
 import { config } from "./wagmi.ts";
-// import { theme } from "./theme.ts";
+import { themeOverrides } from "./theme.ts";
 import { privyConfig } from './privyConfig';
 
 import "./index.css";
-import { CDPReactProvider, type Theme } from "@coinbase/cdp-react";
+import { CDPReactProvider } from "@coinbase/cdp-react";
 
 const queryClient = new QueryClient();
-
-const themeOverrides: Partial<Theme> = {
-   //@ts-ignore
-  "colors-background": "black",
-  "colors-backgroundOverlay": "rgba(0,0,0,0.5)",
-  "colors-text": "white",
-  "colors-textSecondary": "#999999",
-}
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
--- react/src/theme.ts
+++ react/src/theme.ts
@@ -1,20 +1,9 @@
 import { type Theme } from "@coinbase/cdp-react/theme";
 
-export const theme: Partial<Theme> = {
-  "colors-bg-default": "var(--cdp-example-card-bg-color)",
-  "colors-bg-overlay": "var(--cdp-example-bg-overlay-color)",
-  "colors-bg-skeleton": "var(--cdp-example-bg-skeleton-color)",
-  "colors-bg-primary": "var(--cdp-example-accent-color)",
-  "colors-bg-secondary": "var(--cdp-example-bg-low-contrast-color)",
-  "colors-fg-default": "var(--cdp-example-text-color)",
-  "colors-fg-muted": "var(--cdp-example-text-secondary-color)",
-  "colors-fg-primary": "var(--cdp-example-accent-color)",
-  "colors-fg-onPrimary": "var(--cdp-example-accent-foreground-color)",
-  "colors-fg-onSecondary": "var(--cdp-example-text-color)",
-  "colors-line-default": "var(--cdp-example-card-border-color)",
-  "colors-line-heavy": "var(--cdp-example-text-secondary-color)",
-  "colors-line-primary": "var(--cdp-example-accent-color)",
-  "font-family-sans": "var(--cdp-example-font-family)",
-  "font-size-base": "var(--cdp-example-base-font-size)",
-};
-
+export const themeOverrides: Partial<Theme> = {
+   //@ts-ignore
+  "colors-background": "black",
+  "colors-backgroundOverlay": "rgba(0,0,0,0.5)",
+  "colors-text": "white",
+  "colors-textSecondary": "#999999",
+}
