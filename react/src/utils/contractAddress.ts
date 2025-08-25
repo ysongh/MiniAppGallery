422c22b4424654ec5b9ce9859e2497959606dc65
You Song Hou <ysongweb3@gmail.com>
2025-08-25 18:58:47 -0400

    feat: add support for Celo

--- react/src/utils/contractAddress.ts
+++ react/src/utils/contractAddress.ts
@@ -1,15 +1,17 @@
-import { base, baseSepolia, celoSepolia } from "wagmi/chains";
+import { base, celoSepolia, celo } from "wagmi/chains";
 
 export const getContractAddress = (networkId: number): `0x${string}` => {
   if (networkId === 84532) return import.meta.env.VITE_BASESEPOLIA_ONTRACT_ADDRESS as `0x${string}`;
   else if (networkId === 8453) return import.meta.env.VITE_BASE_ONTRACT_ADDRESS as `0x${string}`;
   else if (networkId === 44787) return import.meta.env.VITE_CELOALFAJORES_CONTRACT_ADDRESS as `0x${string}`;
   else if (networkId === 11142220) return import.meta.env.VITE_CELOSEPOLIA_CONTRACT_ADDRESS as `0x${string}`;
+  else if (networkId === 42220) return import.meta.env.VITE_CELO_CONTRACT_ADDRESS as `0x${string}`;
   return import.meta.env.VITE_LOCAL_CONTRACT_ADDRESS as `0x${string}`;;
 }
 
 export const supportedNetworks = [
   { id: base.id, name: 'Base', chain: base },
-  { id: baseSepolia.id, name: 'Base Sepolia', chain: baseSepolia },
+  // { id: baseSepolia.id, name: 'Base Sepolia', chain: baseSepolia },
   { id: celoSepolia.id, name: 'Celo Sepolia', chain: celoSepolia },
+  { id: celo.id, name: 'Celo', chain: celo },
 ];
--- react/src/wagmi.ts
+++ react/src/wagmi.ts
@@ -1,16 +1,17 @@
 import { farcasterFrame } from "@farcaster/frame-wagmi-connector";
 import { createConfig } from '@privy-io/wagmi';
 import { http } from 'wagmi';
-import { base, baseSepolia, celoSepolia, hardhat } from "wagmi/chains";
+import { base, baseSepolia, celoSepolia, celo } from "wagmi/chains";
 
 export const config = createConfig({
-  chains: [base, baseSepolia, celoSepolia, hardhat],
+  chains: [base, baseSepolia, celoSepolia, celo],
   connectors: [farcasterFrame()],
   // @ts-ignore
   transports: {
     [base.id]: http(),
     [baseSepolia.id]: http(),
     [celoSepolia.id]: http(),
+    [celo.id]: http(),
     // [hardhat.id]: http(),
   },
 });
