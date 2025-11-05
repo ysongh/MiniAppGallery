a64fd0326623b233055babdd5749708b1d79040d
You Song Hou <ysongweb3@gmail.com>
2025-11-04 23:12:56 -0500

    feat: add connection to MongoDB

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import pkg from 'body-parser';
+import mongoose from 'mongoose';
 
 import miniappRoutes from './routes/miniapp.js';
 
@@ -12,8 +13,14 @@ const app = express();
 const port = 4000;
 
 app.use(cors());
-
 app.use(json());
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+    .then(() => console.log('Connected to MongoDB'))
+    .catch((err) => console.error('MongoDB connection error:', err));
 
 app.use('/api/miniapp', miniappRoutes);
 app.get('/', (req, res) => res.send('It Work'));
